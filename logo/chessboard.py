58f4cd74bb006b3c20a1e9558aa4a879f9962ef6
mauriceyap <maurice@mauriceyap.co.uk>
2014-08-19 12:24:05 +0100

    Running logo/chessboard.py

--- logo/chessboard.py
+++ logo/chessboard.py
@@ -8,20 +8,19 @@ revColors = ["black", "yellow"]
 
 
 
-
-
-for i in range(0,4):
- color (colors [0])
- move (50)
- color (colors [1])
- move (50)
+while n <50:
+ for i in range(0,4):
+  color (colors [0])
+  move (50)
+  color (colors [1])
+  move (50)
   
-turn (90)
-move (1)
-turn (90)
+ turn (90)
+ move (1)
+ turn (90)
 
-for i in range(0,4):
- color (colors [1])
- move (50)
- color (colors [0])
- move (50)
+ for i in range(0,4):
+  color (colors [1])
+  move (50)
+  color (colors [0])
+  move (50)
