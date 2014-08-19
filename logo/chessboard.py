cdd78ac0a6f7720af3a63bcc53cf6801f3f56e97
mauriceyap <maurice@mauriceyap.co.uk>
2014-08-19 12:06:51 +0100

    Running logo/chessboard.py

--- logo/chessboard.py
+++ logo/chessboard.py
@@ -4,10 +4,8 @@ from tealight.logo import (move,
 
 colors = ["black", "yellow"]
 
-for i in the range (0,200):
-  
-  for i in range(0,4):
-    color ("black")
-    move (50)
-    color ("yellow")
-    move (50)
+for i in range(0,4):
+ color ("black")
+ move (50)
+ color ("yellow")
+ move (50)
