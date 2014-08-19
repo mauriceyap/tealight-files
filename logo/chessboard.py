e574f3a8d30fa0d2c5ed7c942b451b6d5a3af124
mauriceyap <maurice@mauriceyap.co.uk>
2014-08-19 12:15:55 +0100

    Running logo/chessboard.py

--- logo/chessboard.py
+++ logo/chessboard.py
@@ -8,7 +8,7 @@ n=1
 
 while n < 100:
 
- for i in range(0,4):
+ for i in range(0,100):
   color ("black")
   move (50)
   color ("yellow")
@@ -25,6 +25,16 @@ while n < 100:
   move (50)
   color ("yellow")
   move (50)
+  turn (90)
+  move (1)
+  turn (90)
+  color ("yellow")
+  move (50)
+  color ("black")
+  move (50)
+  turn (270)
+  move (1)
+  turn (270)
   n = n + 1
   
   
