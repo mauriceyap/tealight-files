5539760c377c67f9ceeaa95b19ae407ba8e61930
mauriceyap <maurice@mauriceyap.co.uk>
2014-08-19 15:05:27 +0100

    Running robot/maze.py

--- robot/maze.py
+++ robot/maze.py
@@ -10,6 +10,9 @@ for i in range (0,100):
  if left_side() == 'wall':
    move()
    
- if touch() == 'wall':
-   turn(1)
+ if touch() == 'wall', left_side() == 'None':
+   turn(-1)
+      
+ if touch() == 'wall', left_side() == 'None':
+    turn(-1)
 
