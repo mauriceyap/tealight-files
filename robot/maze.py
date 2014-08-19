6e38fea200e4a8b2c5e77531165768b39ddffe7e
mauriceyap <maurice@mauriceyap.co.uk>
2014-08-19 15:09:14 +0100

    Running robot/maze.py

--- robot/maze.py
+++ robot/maze.py
@@ -7,6 +7,12 @@ from tealight.robot import (move,
                             right_side)
 
 for i in range (0,100):
- if left_side() == 'wall' and touch() == 'None':
+ if left_side() == 'wall' and touch() == None:
    move()
+   
+ if touch() == 'wall' and left_side() == 'None':
+   turn(-1)
+      
+ if touch() == 'wall' and right_side() == 'None':
+    turn(1)
 
