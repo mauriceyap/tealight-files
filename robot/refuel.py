63c636e5adb861bf4a9b402496fb6857ed9d7784
mauriceyap <maurice@mauriceyap.co.uk>
2014-08-19 14:39:56 +0100

    Running robot/refuel.py

--- robot/refuel.py
+++ robot/refuel.py
@@ -6,10 +6,5 @@ from tealight.robot import (move,
                             left_side, 
                             right_side)
 
-move(3)
-turn(1)
-move(3)
-turn(1)
-move(7)
-turn(-1)
-move(6)
+move (1)
+smell
