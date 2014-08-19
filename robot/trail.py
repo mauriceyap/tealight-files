205fe1f1933cd7920b3886729ae060adc7639a56
mauriceyap <maurice@mauriceyap.co.uk>
2014-08-19 14:45:05 +0100

    Running robot/trail.py

--- robot/trail.py
+++ robot/trail.py
@@ -5,6 +5,6 @@ from tealight.robot import (move,
                             smell, 
                             left_side, 
                             right_side)
-for i in range (0,100)
+for n in range (0,100)
  if touch() == 'fruit':
   move()
