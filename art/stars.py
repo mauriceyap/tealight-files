82bd165f48e20ee9eb00be644331efba098ca713
mauriceyap <maurice@mauriceyap.co.uk>
2014-08-20 10:19:04 +0100

    Running art/stars.py

--- art/stars.py
+++ art/stars.py
@@ -9,8 +9,8 @@ def ellipse(x, y, c, size, spines):
   angle = 0
   
   for i in range(0, spines):
-    x0 = x + (size * cos(angle))
-    y0 = y + (size * sin(angle))
+    x0 = x+2 + (size * cos(angle))
+    y0 = y+2 + (size * sin(angle))
     
     line(x, y, x0, y0)
     
