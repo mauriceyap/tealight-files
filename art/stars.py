522152c05fabc7ddd846443fea51ae53a177ba6e
mauriceyap <maurice@mauriceyap.co.uk>
2014-08-20 10:15:59 +0100

    Running art/stars.py

--- art/stars.py
+++ art/stars.py
@@ -16,6 +16,6 @@ def ellipse(x, y, c, size, spines):
     
     angle = angle + (2 * pi / spines)
 
-ellipse(300, 300, "blue", 100, 2)
-ellipse(600, 400, "purple", 200, 2)
-ellipse(450, 200, "orange", 125, 2)
+ellipse(300, 300, "blue", 100, 1)
+ellipse(600, 400, "purple", 200, 1)
+ellipse(450, 200, "orange", 125, 1)
