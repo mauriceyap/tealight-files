fc34d4e8d264765e1282dc31bfb2df2121a66c2b
mauriceyap <maurice@mauriceyap.co.uk>
2014-08-21 12:29:37 +0100

    Running art/maths mechanism.py

--- art/maths mechanism.py
+++ art/maths mechanism.py
@@ -131,7 +131,7 @@ for i in range (0,4):
 row=5
 col=1
 
-for i in range (0,5):
+for i in range (0,4):
     if boardArray [row][col] ==boardArray [row-1][col+1]==boardArray [row-2][col+2]==boardArray [row-3][col+3]==1:
       winnerRed = 1
 
