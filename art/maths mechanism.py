630c457c565c042f116448e9c447919c96b06869
mauriceyap <maurice@mauriceyap.co.uk>
2014-08-21 10:47:10 +0100

    Running art/maths mechanism.py

--- art/maths mechanism.py
+++ art/maths mechanism.py
@@ -7,10 +7,9 @@ boardArray =[[44,22,1,1,1,1,1,1],
              [1,1,1,1,1,1,1,1],
              [1,1,1,1,1,1,1,1]]
 
-print boardArray
 
-row = 1
+row = 3
 col = 1
 
-if boardArray [row][col] ==9:
+if boardArray [row][col] ==boardArray [row+1][col]boardArray [row+2][col]boardArray [row+3][col]:
   print 'winner'
