f075ac2a0aad11520cc72f46c97b4166f140948f
mauriceyap <maurice@mauriceyap.co.uk>
2014-08-21 10:38:07 +0100

    Running art/maths mechanism.py

--- art/maths mechanism.py
+++ art/maths mechanism.py
@@ -6,4 +6,5 @@ boardArray =[[44,22,1,1,1,1,1,1],
              [1,1,1,1,1,1,1,1],
              [1,1,1,1,1,1,1,1],
              [1,1,1,1,1,1,1,1]]
-print boardArray [0][1]
+boardArray [0][0]=43
+print boardArray [0][0]
