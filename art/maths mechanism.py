7dea31c6b8c862aaa0d370fa4e53787c261d344a
mauriceyap <maurice@mauriceyap.co.uk>
2014-08-21 10:58:24 +0100

    Running art/maths mechanism.py

--- art/maths mechanism.py
+++ art/maths mechanism.py
@@ -2,15 +2,23 @@ boardArray =[[44,22,1,1,1,1,1,1],
              [1,9,1,1,1,1,1,1],
              [1,1,1,1,1,1,1,1],
              [1,1,1,1,1,1,1,1],
-             [1,1,1,1,1,1,1,1],
-             [1,1,1,1,1,1,1,1],
-             [1,1,1,1,1,1,1,1],
+             [1,r,1,1,1,1,1,1],
+             [1,r,1,1,1,1,1,1],
+             [1,r,1,1,1,1,1,1],
              [1,1,1,1,1,1,1,1]]
 
 
-boardArray [3][1] =6
+boardArray [3][1] =r
 row = 3
 col = 1
 
-if boardArray [row][col] ==boardArray [row +1][col]==boardArray [row +2][col]==boardArray [row+3][col]:
-  print 'winner'
+if boardArray [row][col] ==boardArray [row +1][col]==boardArray [row +2][col]==boardArray [row+3][col]==r:
+  winnerRed = 1
+
+if boardArray [row][col] ==boardArray [row +1][col]==boardArray [row +2][col]==boardArray [row+3][col]==y:
+  winnerYellow = 1
+  
+if winnerRed = 1:
+  print 'Red is the winner'
+if winnerYellow = 1:
+  print 'Yellow is the winner'
