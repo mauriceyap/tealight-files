630ddac2f4179490e957fe368b984a6ebb131040
mauriceyap <maurice@mauriceyap.co.uk>
2014-08-21 11:06:29 +0100

    Running art/maths mechanism.py

--- art/maths mechanism.py
+++ art/maths mechanism.py
@@ -7,20 +7,21 @@ boardArray =[[0,0,0,0,0,0,0,0],
              [0,0,0,0,0,0,0,0],
              [0,0,0,0,0,0,0,0],
              [0,0,0,0,0,0,0,0],
-             [0,0,0,0,0,0,0,0],
+             [1,1,1,1,0,0,0,0],
              [0,0,0,0,0,0,0,0]]
 
 
-boardArray [3][1] ='y'
-row = 3
-col = 1
 
+row = 0
+col = 0
 
-if boardArray [row][col] ==boardArray [row +1][col]==boardArray [row +2][col]==boardArray [row+3][col]=='r':
-  winnerRed = 1
+for i<8:
+  if boardArray [row][col] ==boardArray [row +1][col]==boardArray [row +2][col]==boardArray [row+3][col]==1:
+    winnerRed = 1
 
-if boardArray [row][col] ==boardArray [row +1][col]==boardArray [row +2][col]==boardArray [row+3][col]=='y':
-  winnerYellow = 1
+  if boardArray [row][col] ==boardArray [row +1][col]==boardArray [row +2][col]==boardArray [row+3][col]==2:
+    winnerYellow = 1
+  row = row + 1
   
 if winnerRed == 1:
   print 'Red is the winner'
