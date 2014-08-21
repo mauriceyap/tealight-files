d83b857c9dd0c28fa548cad7e958ea031aa2a02a
mauriceyap <maurice@mauriceyap.co.uk>
2014-08-21 10:58:32 +0100

    Running art/maths mechanism.py

--- art/maths mechanism.py
+++ art/maths mechanism.py
@@ -18,7 +18,7 @@ if boardArray [row][col] ==boardArray [row +1][col]==boardArray [row +2][col]==b
 if boardArray [row][col] ==boardArray [row +1][col]==boardArray [row +2][col]==boardArray [row+3][col]==y:
   winnerYellow = 1
   
-if winnerRed = 1:
+if winnerRed == 1:
   print 'Red is the winner'
-if winnerYellow = 1:
+if winnerYellow == 1:
   print 'Yellow is the winner'
