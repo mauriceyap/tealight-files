cfdb4959f6cfda2b57221971c709fc9e9c6557f0
mauriceyap <maurice@mauriceyap.co.uk>
2014-08-21 11:12:23 +0100

    Running art/maths mechanism.py

--- art/maths mechanism.py
+++ art/maths mechanism.py
@@ -1,7 +1,7 @@
 winnerRed=0
 winnerYellow=0
 
-boardArray =[[0,0,0,1,0,0,0,0],
+boardArray =[[0,0,0,0,0,0,0,0],
              [0,0,0,1,0,0,0,0],
              [0,0,0,1,0,0,0,0],
              [0,0,0,1,0,0,0,0],
@@ -16,11 +16,13 @@ row = 0
 col = 0
 
 for i in range (0,8):
-  if boardArray [row][col] ==boardArray [row +1][col]==boardArray [row +2][col]==boardArray [row+3][col]==1:
-    winnerRed = 1
+  for j in range (0,4):
+    if boardArray [row][col] ==boardArray [row +1][col]==boardArray [row +2][col]==boardArray [row+3][col]==1:
+      winnerRed = 1
 
-  if boardArray [row][col] ==boardArray [row +1][col]==boardArray [row +2][col]==boardArray [row+3][col]==2:
-    winnerYellow = 1
+    if boardArray [row][col] ==boardArray [row +1][col]==boardArray [row +2][col]==boardArray [row+3][col]==2:
+      winnerYellow = 1
+    row = row+1
   col = col + 1
   
   
