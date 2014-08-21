5285ce92102e2aad03cb15a1361dda83d93387f7
mauriceyap <maurice@mauriceyap.co.uk>
2014-08-21 10:47:56 +0100

    Running art/maths mechanism.py

--- art/maths mechanism.py
+++ art/maths mechanism.py
@@ -11,5 +11,5 @@ boardArray =[[44,22,1,1,1,1,1,1],
 row = 3
 col = 1
 
-if boardArray [row][col] ==boardArray [row+1][col]boardArray [row+2][col]boardArray [row+3][col]:
+if boardArray [row][col] ==boardArray [row +1][col]==boardArray [row +2][col]==boardArray [row+3][col]:
   print 'winner'
