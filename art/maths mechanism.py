c627985ac42d8762208f3408981a6b9dd6a37953
mauriceyap <maurice@mauriceyap.co.uk>
2014-08-21 11:20:38 +0100

    Running art/maths mechanism.py

--- art/maths mechanism.py
+++ art/maths mechanism.py
@@ -22,32 +22,7 @@ for i in range (0,8):
     if boardArray [row][col] ==boardArray [row +1][col]==boardArray [row +2][col]==boardArray [row+3][col]==2:
       winnerYellow = 1
     col = col + 1
-row=1
-for i in range (0,8):
-    if boardArray [row][col] ==boardArray [row +1][col]==boardArray [row +2][col]==boardArray [row+3][col]==1:
-      winnerRed = 1
 
-    if boardArray [row][col] ==boardArray [row +1][col]==boardArray [row +2][col]==boardArray [row+3][col]==2:
-      winnerYellow = 1
-    col = col + 1
-row=row+1
-for i in range (17,24):
-    if boardArray [row][col] ==boardArray [row +1][col]==boardArray [row +2][col]==boardArray [row+3][col]==1:
-      winnerRed = 1
-
-    if boardArray [row][col] ==boardArray [row +1][col]==boardArray [row +2][col]==boardArray [row+3][col]==2:
-      winnerYellow = 1
-    col = col + 1
-row=row+1
-for i in range (25,32):
-    if boardArray [row][col] ==boardArray [row +1][col]==boardArray [row +2][col]==boardArray [row+3][col]==1:
-      winnerRed = 1
-
-    if boardArray [row][col] ==boardArray [row +1][col]==boardArray [row +2][col]==boardArray [row+3][col]==2:
-      winnerYellow = 1
-    col = col + 1
-row=row+1
-  
   
 if winnerRed == 1:
   print 'Red is the winner'
