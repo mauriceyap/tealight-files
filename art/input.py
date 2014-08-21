78f8d8c5e559f61f58c46e69001da051985e3910
mauriceyap <maurice@mauriceyap.co.uk>
2014-08-21 12:04:55 +0100

    Closing art/input.py

--- art/input.py
+++ art/input.py
@@ -8,3 +8,19 @@ def handle_mousemove(x,y,button):
   if button == "left":
     color("red")
     circle(x,y,10)
+#new
+
+turn=1
+
+while n<64:
+  def handle_mousemove(x,y,button):
+  if button == 'left':
+    turn = 2
+  elif button == 'left':
+    turn = 1
+  n=n+1
+  
+if turn = 1:
+  color(
+    
+
