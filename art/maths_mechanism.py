fa2797b086c82849e8c5e0c8610a665f99f03773
mauriceyap <maurice@mauriceyap.co.uk>
2014-08-22 09:14:00 +0100

    Running art/maths_mechanism.py

--- art/maths_mechanism.py
+++ art/maths_mechanism.py
@@ -28,3 +28,35 @@ turn=1
   
 
 print check_winner(boardArray)
+
+if check_winner =="red":
+	color("red")
+	spot(445,500,150)
+
+	color("black")
+	text(390,480,"Red Wins!!!")
+	image(310,490,"misc/WhiteBalloon.png")
+	image(325,400,"misc/BlackBalloon.png")
+	image(375,360,"misc/BlueBalloon.png")
+	image(450,360,"misc/GreenBalloon.png")
+	image(375,540,"misc/OrangeBalloon.png")
+	image(450,540,"misc/PinkBalloon.png")
+	image(510,400,"misc/RedBalloon.png")
+	image(510,510,"misc/YellowBalloon.png")
+
+if check_winner =="red":
+	color("yellow")
+	spot(445,500,150)
+	circle (445,500,170)
+
+	color("black")
+	text(390,480,"Yellow Wins!!!")
+	image(310,490,"misc/WhiteBalloon.png")
+	image(325,400,"misc/BlackBalloon.png")
+	image(375,360,"misc/BlueBalloon.png")
+	image(450,360,"misc/GreenBalloon.png")
+	image(375,540,"misc/OrangeBalloon.png")
+	image(450,540,"misc/PinkBalloon.png")
+	image(510,400,"misc/RedBalloon.png")
+	image(510,510,"misc/YellowBalloon.png")
+    
