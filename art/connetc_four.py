3b542d0d2bc575b1f61e07b6b0beddd05e61ec27
mauriceyap <maurice@mauriceyap.co.uk>
2014-08-22 10:08:37 +0100

    Running art/connetc_four.py

--- art/connetc_four.py
+++ art/connetc_four.py
@@ -1,1 +1,51 @@
-print "A new file!"
+from tealight.art import (color, line, spot, circle, box, image, text, background)
+
+from github.mauriceyap.art.maths_mechanism import *
+
+
+turn = 0
+cell_size = 100
+
+cells_x = 8
+cells_y = 8
+
+offset_x = 20
+offset_y = 80
+
+def handle_mousedown(x,y,button):
+  global turn
+  
+  col = (x-offset_x) / cell_size
+  
+  if 0 <= col < 8:
+    
+    drop_counter(col,turn+1)
+    
+    turn = (turn + 1) % 2
+    
+    draw()
+    
+    
+    
+def draw():
+  color("blue")
+  box(offset_x,offset_y,cell_size * cells_x,cell_size * cells_y)
+  
+  width = 20
+  height = 8
+  
+  for i in range(0,cells_x):
+    for j in range(0,cells_y):
+  
+      if boardArray [j][i] == 0:
+        color("white")
+      elif boardArray[j][i] == 1:
+        color("red")
+      elif boardArray[j][i] == 2:
+        color("yellow")
+      
+      spot(offset_x + i * cell_size + cell_size/2, offset_y + j * cell_size + cell_size/2, cell_size *0.4)
+  
+  check_winner(boardArray)
+
+draw()
