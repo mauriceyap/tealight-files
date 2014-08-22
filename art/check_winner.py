a40189c3befab9d832c89abba8a647b47e09e27d
mauriceyap <maurice@mauriceyap.co.uk>
2014-08-22 09:42:43 +0100

    Running art/check_winner.py

--- art/check_winner.py
+++ art/check_winner.py
@@ -584,44 +584,36 @@ def check_winner(boardArray):
   #2declaration  
   if winnerRed == 1:
     print 'Red is the winner'
+    color("red")
+    spot(445,500,150)
+  
+    color("black")
+    text(390,480,"Red Wins!!!")
+    image(310,490,"misc/WhiteBalloon.png")
+    image(325,400,"misc/BlackBalloon.png")
+    image(375,360,"misc/BlueBalloon.png")
+    image(450,360,"misc/GreenBalloon.png")
+    image(375,540,"misc/OrangeBalloon.png")
+    image(450,540,"misc/PinkBalloon.png")
+    image(510,400,"misc/RedBalloon.png")
+    image(510,510,"misc/YellowBalloon.png")
     return "red"
   if winnerYellow == 1:
     print 'Yellow is the winner'
+    color("yellow")
+    spot(445,500,150)
+    circle (445,500,170)
+  
+    color("black")
+    text(390,480,"Yellow Wins!!!")
+    image(310,490,"misc/WhiteBalloon.png")
+    image(325,400,"misc/BlackBalloon.png")
+    image(375,360,"misc/BlueBalloon.png")
+    image(450,360,"misc/GreenBalloon.png")
+    image(375,540,"misc/OrangeBalloon.png")
+    image(450,540,"misc/PinkBalloon.png")
+    image(510,400,"misc/RedBalloon.png")
+    image(510,510,"misc/YellowBalloon.png")
     return "yellow"
   
   return None
-
-#1Graphics
-
-if check_winner =="red":
-	color("red")
-	spot(445,500,150)
-
-	color("black")
-	text(390,480,"Red Wins!!!")
-	image(310,490,"misc/WhiteBalloon.png")
-	image(325,400,"misc/BlackBalloon.png")
-	image(375,360,"misc/BlueBalloon.png")
-	image(450,360,"misc/GreenBalloon.png")
-	image(375,540,"misc/OrangeBalloon.png")
-	image(450,540,"misc/PinkBalloon.png")
-	image(510,400,"misc/RedBalloon.png")
-	image(510,510,"misc/YellowBalloon.png")
-
-if check_winner =="yellow":
-	color("yellow")
-	spot(445,500,150)
-	circle (445,500,170)
-
-	color("black")
-	text(390,480,"Yellow Wins!!!")
-	image(310,490,"misc/WhiteBalloon.png")
-	image(325,400,"misc/BlackBalloon.png")
-	image(375,360,"misc/BlueBalloon.png")
-	image(450,360,"misc/GreenBalloon.png")
-	image(375,540,"misc/OrangeBalloon.png")
-	image(450,540,"misc/PinkBalloon.png")
-	image(510,400,"misc/RedBalloon.png")
-	image(510,510,"misc/YellowBalloon.png")
-    
-  #End Winner Checker
