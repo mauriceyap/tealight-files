f50b5488d63c2f25b06624ea47b52fc94d7976f9
mauriceyap <maurice@mauriceyap.co.uk>
2015-01-28 14:17:45 +0000

    Running art/untitled.py

--- art/untitled.py
+++ art/untitled.py
@@ -6,7 +6,9 @@ v= (( (a**2)-(2**6) )**(1/6)) + (( (b**2)-(b**6) )**(1/6))
 for i in range(1,n):
    v += 2*  ( ((a+(i*h))**2)-((a+(i*h))**6) )**(1/6)
    print v     
-t=(h/2.0)*v
+half=(h/2.0)
+print half
+t=half*v
 f=2**0.5
 print t
 print h
