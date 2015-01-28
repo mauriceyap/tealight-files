d655c18cd1d4e086196b2676eb426f0a8f7b6e03
mauriceyap <maurice@mauriceyap.co.uk>
2015-01-28 16:07:26 +0000

    Running art/trap butterfly.py

--- art/trap butterfly.py
+++ art/trap butterfly.py
@@ -1,8 +1,9 @@
-def trapeziumRule(a,b,n):
+def trapRule(a,b,n):
   h=(b-a)/n
   v= (( (a**2)-(a**6) )**(1/6.0)) + (( (b**2)-(b**6) )**(1/6.0))
   for i in range(1,n):
      v += 2*  ( ((a+(i*h))**2)-((a+(i*h))**6) )**(1/6.0)
-      
+        
   t=(h/2.0)*v
-return t*4
+  return t*4
+print trapRule(1,0,1024)
