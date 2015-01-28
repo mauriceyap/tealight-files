ec36c2f67a8caa5f9a432d31c4519c842d37ac3b
mauriceyap <maurice@mauriceyap.co.uk>
2015-01-28 16:58:08 +0000

    Running art/trap butterfly.py

--- art/trap butterfly.py
+++ art/trap butterfly.py
@@ -1,5 +1,5 @@
 #Trapezium rule
-def trapRule(a,b,n):
+def trap(a,b,n):
   h=(b-a)/n
   v= (( (a**2)-(a**6) )**(1/6.0)) + (( (b**2)-(b**6) )**(1/6.0))
   u=0
@@ -12,19 +12,19 @@ def trapRule(a,b,n):
 
   
 #Mid ordinate rule  
-def midOrdRule(a,b,n):
+def midOrd(a,b,n):
   h=(b-a)/n
   v=0.0
   u=0
   for i in range(1,n-1):
     u=(a+ (((2.0*i)-1.0)*h) /2.0)
     v += (( (u**2)-(u**6) )**(1/6.0))
-  return v*h*4
+  return v*h
 
 #Output
 for x in xrange(6):
-  print 'M',(2**x),'=',midOrdRule(0.0,1,(2**x))
-  print 'T',(2**x),'=',trapRule(0.0,1,(2**x))
+  print 'M',(2**x),'=',midOrd(0.0,1,(2**x))
+  print 'T',(2**x),'=',trap(0.0,1,(2**x))
     
  
  
