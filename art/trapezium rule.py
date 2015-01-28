66b679c60d67830172875635f21e3036a41049aa
mauriceyap <maurice@mauriceyap.co.uk>
2015-01-28 16:03:53 +0000

    Running art/trapezium rule.py

--- art/trapezium rule.py
+++ art/trapezium rule.py
@@ -1,10 +1,12 @@
-a=0
-b=0.5
-n=5
-h=(b-a)/n
-v=((1+(a**3))**0.5)+((1+(b**3))**0.5)
-for i in range(1,n):
-   v += 2*((1+(a+(i*h))**3)**0.5)
-t=(h/2)*v
-f=2**0.5
-print 'Trap rule for n=',n,'is',t
+def trapeziumRule(a,b,n):
+  a=0
+  b=0.5
+  n=5
+  h=(b-a)/n
+  v=((1+(a**3))**0.5)+((1+(b**3))**0.5)
+  for i in range(1,n):
+     v += 2*((1+(a+(i*h))**3)**0.5)
+  t=(h/2)*v
+  f=2**0.5
+  return t
+print t
