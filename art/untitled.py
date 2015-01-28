39e575318d50e263e036eaf95a269fe4bc61bb67
mauriceyap <maurice@mauriceyap.co.uk>
2015-01-28 14:17:07 +0000

    Running art/untitled.py

--- art/untitled.py
+++ art/untitled.py
@@ -1,8 +1,12 @@
-a=0
+a=0.0
 b=1
-n=10
-h=(b - a)/n
+n=256
+h=(b-a)/n
+v= (( (a**2)-(2**6) )**(1/6)) + (( (b**2)-(b**6) )**(1/6))
+for i in range(1,n):
+   v += 2*  ( ((a+(i*h))**2)-((a+(i*h))**6) )**(1/6)
+   print v     
+t=(h/2.0)*v
+f=2**0.5
+print t
 print h
-print b
-d=1/10
-print d
