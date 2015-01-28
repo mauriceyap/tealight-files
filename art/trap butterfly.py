f566f3063402eb058aea0d934c6cde8cf87dd13c
mauriceyap <maurice@mauriceyap.co.uk>
2015-01-28 14:25:20 +0000

    Running art/trap butterfly.py

--- art/trap butterfly.py
+++ art/trap butterfly.py
@@ -1,1 +1,12 @@
-print "A new file!"
+a=0.0
+b=1
+n=2048
+h=(b-a)/n
+v= (( (a**2)-(a**6) )**(1/6.0)) + (( (b**2)-(b**6) )**(1/6.0))
+for i in range(1,n):
+   v += 2*  ( ((a+(i*h))**2)-((a+(i*h))**6) )**(1/6.0)
+    
+t=(h/2.0)*v
+trap=t*4
+print t
+print trap
