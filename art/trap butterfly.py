e8891d911d521fa60b48f2fc893cbfe8006ec9f7
mauriceyap <maurice@mauriceyap.co.uk>
2015-01-28 16:44:43 +0000

    Running art/trap butterfly.py

--- art/trap butterfly.py
+++ art/trap butterfly.py
@@ -15,8 +15,8 @@ for x in xrange(12):
 def midOrdRule(a,b,n):
   h=(b-a)/n
   v=0.0
-  for x in xrange(n):
-    u=(   a+ (((2*x)-1)(h/2.0))   )
+  for i in range(1,n):
+    u=(   a+ (((2*i)-1)(h/2.0))   )
     v += (( (u**2)-(u**6) )**(1/6.0))
   return v
 
